ef2d7c64ba320ae645ed443ad665cc5309f9fdd4
Maccabee Levine <msl321@lehigh.edu>
2023-05-10 17:11:58 +0000

    Test record views that cover various special cases.
    - "normal"
    - non-requestable
    - summary holdings
    - restricted journals
    - bound-with
    - finding aids

--- scripts/test.js
+++ scripts/test.js
@@ -4,6 +4,8 @@ var expect = require('chai').expect;
 
 const { Builder, By, Key } = require("selenium-webdriver");
 
+const environments = require('../config/config');
+
 const service = new chrome.ServiceBuilder('chromedriver'); 
 
 const driver = new Builder()
@@ -12,14 +14,59 @@ const driver = new Builder()
   .setChromeOptions(new chrome.Options().addArguments( ['--headless','--no-sandbox']))
   .build();
 
-describe('browser', function() {
-  this.timeout(5000);
+describe('Browser-based tests', function() {
+  this.timeout(10000);
 
-  it('basic', async function() {
-    await driver.manage().setTimeouts({implicit: 10000});
-    await driver.get("https://www.bing.com");
-    let title = await driver.getTitle();
-    expect(title).to.equal("Bing"); 
+  environments.forEach(({name, url_prefix}) => {
+
+    describe(name, function() {
+
+      it('Homepage', async function() {
+        await driver.get(url_prefix);
+        let title = await driver.getTitle();
+        expect(title).to.equal("Search Home"); 
+      });
+
+      describe('Normal print record page', function() {
+
+        before(async function() {
+          await driver.get(url_prefix + '/Record/12345');
+        });
+
+        it('Has a request link', async function() {
+          await driver.findElement(By.css('.holdings-tab .placehold'));
+        });
+
+      });
+
+      it('Un-Requestable Material', async function() {
+        await driver.get(url_prefix + '/Record/677843');
+        let request_links = await driver.findElements(By.css('.holdings-tab .placehold'));
+        expect(request_links).to.be.empty;
+      });
+
+      it('Summary Holdings', async function() {
+        await driver.get(url_prefix + '/Record/742590');
+        await driver.findElement(By.xpath('//h3[text()="Summary Holdings"]'));
+      });
+
+      describe('Restricted Journal', function() {
+        it('Not Logged In', async function() {
+          await driver.get(url_prefix + '/Record/12639');
+          await driver.findElement(By.css('.holdings-tab #loginOptions'));
+        });
+      });
+
+      it('Bound-with', async function() {
+        await driver.get(url_prefix + '/Record/1092692');
+        await driver.findElement(By.xpath('//h3[text()="This item is bound with: "]'));
+      });
+
+      it('Finding Aid tab', async function() {
+        await driver.get(url_prefix + '/Record/10664764');
+        await driver.findElement(By.css('.record-tab.findingaid'));
+      });
+
+    });
   });
-
 });
