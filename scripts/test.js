5cc3ca1a86c66dc9d8801270b4f3b0963a7e490c
Maccabee Levine <msl321@lehigh.edu>
2023-05-10 18:36:20 +0000

    Confirm each page loads without error

--- scripts/test.js
+++ scripts/test.js
@@ -23,6 +23,7 @@ describe('Browser-based tests', function() {
 
       it('Homepage', async function() {
         await driver.get(url_prefix);
+        await expectPageRenders();
         let title = await driver.getTitle();
         expect(title).to.equal("Search Home"); 
       });
@@ -31,6 +32,7 @@ describe('Browser-based tests', function() {
 
         before(async function() {
           await driver.get(url_prefix + '/Record/12345');
+          await expectPageRenders();
         });
 
         it('Has a request link', async function() {
@@ -41,32 +43,41 @@ describe('Browser-based tests', function() {
 
       it('Un-Requestable Material', async function() {
         await driver.get(url_prefix + '/Record/677843');
+        await expectPageRenders();
         let request_links = await driver.findElements(By.css('.holdings-tab .placehold'));
         expect(request_links).to.be.empty;
       });
 
       it('Summary Holdings', async function() {
         await driver.get(url_prefix + '/Record/742590');
+        await expectPageRenders();
         await driver.findElement(By.xpath('//h3[text()="Summary Holdings"]'));
       });
 
       describe('Restricted Journal', function() {
         it('Not Logged In', async function() {
           await driver.get(url_prefix + '/Record/12639');
+          await expectPageRenders();
           await driver.findElement(By.css('.holdings-tab #loginOptions'));
         });
       });
 
       it('Bound-with', async function() {
         await driver.get(url_prefix + '/Record/1092692');
+        await expectPageRenders();
         await driver.findElement(By.xpath('//h3[text()="This item is bound with: "]'));
       });
 
       it('Finding Aid tab', async function() {
         await driver.get(url_prefix + '/Record/10664764');
+        await expectPageRenders();
         await driver.findElement(By.css('.record-tab.findingaid'));
       });
 
     });
   });
+
+  function expectPageRenders() {
+    return driver.findElement(By.id('loginOptions'));
+  }
 });
