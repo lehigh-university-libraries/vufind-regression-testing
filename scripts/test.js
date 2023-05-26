8c4a5aef3e3faebf970fe93a5c31cf4cceb74c61
Maccabee Levine <msl321@lehigh.edu>
2023-05-26 13:09:57 +0000

    Text 'exclude' facet formatting

--- scripts/test.js
+++ scripts/test.js
@@ -182,6 +182,15 @@ describe('Browser-based tests', function() {
               });
 
             });
+
+            it('Exclude facet (call number)', async function() {
+              // Result count has no "badge" class
+              let badge_elements = await driver.findElements(By.css("#side-collapse-dewey-hundreds .facet span.badge"));
+              expect(badge_elements).to.be.empty;
+
+              // Result count has a tooltip
+              await driver.findElement(By.css("#side-collapse-dewey-hundreds .facet a[title*='Exclude']"));
+            });
   
           });
 
